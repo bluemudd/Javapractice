055ba0ed76d2a049e776409efbbc743c9654e17c
bluemudd <parkjm0817@gmail.com>
2022-06-21 18:04:27 +0900

    백준 1330

---
+++ javaPract/src/javaPract/Day4Ex3.java
@@ -0,0 +1,20 @@
+package javaPract;
+
+import java.util.Scanner;
+public class Day4Ex3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a=sc.nextInt();
+		b=sc.nextInt();
+		if (a > b){
+			System.out.println(">");
+		}
+		else if(a < b) {
+			System.out.println("<");
+		}
+		else {
+			System.out.println("==");
+		}
+	}
+}
